b5575a48f7b8b2234437d1f7d108ff7cc0800a63
RenanFCa <renan.calazans@uni9.edu.br>
2024-09-27 10:59:27 -0300

    Add files via upload

--- sript5.js
+++ sript5.js
@@ -3,8 +3,9 @@ let saida = document.getElementById("saida");
 a=50; // int
 b=120; // int
 c=200; // int
-f="";
+f=(a%2==0)?"PAR": "IMPAR"; // f = 
 d=(a<=b) ? "Verdadeiro" : "Falso"; //  d = ( true)
 e=(a>=c) ? "Verdadeiro" : "Falso"; // e = ( false)
 saida.innerHTML=`d = ${d}<br>`; // 
 saida.innerHTML+=`e = ${e}`; // 
+saida.innerHTML+=`a = ${a} - é ${f}`; 
